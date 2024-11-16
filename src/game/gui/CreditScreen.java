54c9bae1eb5eb010f21f704bb6f410b2c30c84b0
aaronmorenomarino <aaronmorenomarino@gmail.com>
2024-11-16 15:25:17 -0600

    credit

---
+++ src/game/gui/CreditScreen.java
@@ -0,0 +1,53 @@
+package game.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.*;
+
+public class CreditScreen extends JFrame {
+	
+	public CreditScreen()
+	{
+		super("Credit Screen");
+		setLayout(null);
+		// Creating a new label of title
+        JLabel credit = new JLabel();
+        
+        JLabel text = new JLabel("Made by SIGMA CODERS");
+        
+
+        // Creating instance of JButton
+        JButton back = new JButton(" Back ");
+
+        // x axis, y axis, width, height
+        back.setBounds(150, 300, 200, 50);
+        text.setBounds(50,50,100,50);
+
+        // Adding actions to buttons
+        back.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		new TitleScreen();
+        		CreditScreen.this.dispose();
+
+        	}
+        });
+
+        // adding buttons in JFrame
+        add(back);
+        credit.add(text);
+        
+        //adding text in JFrames
+        
+        
+        // 400 width and 500 height
+        setSize(500, 600);
+
+
+        // making the frame visible
+        setVisible(true);
+	}
+	
+
+}
