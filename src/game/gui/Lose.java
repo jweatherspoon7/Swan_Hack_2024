529a7a7d39fe71ac1db7cfc17ce739a1e48b5d09
Jackson W <90067155+jweatherspoon7@users.noreply.github.com>
2024-11-17 10:51:45 -0600

    win and lose sound

--- src/game/gui/Lose.java
+++ src/game/gui/Lose.java
@@ -1,7 +1,14 @@
 package game.gui;
 
 import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -9,9 +16,13 @@ import javax.swing.JLabel;
 	
 	public class Lose extends JFrame{
 		
+		Clip clip;
+		
 		public Lose(){
 		
 			super("Lose");
+			
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
 			ImageIcon gojo_loses = new ImageIcon("animation (2).gif");
 			 
@@ -26,7 +37,20 @@ import javax.swing.JLabel;
 		     setExtendedState(JFrame.MAXIMIZED_BOTH);
 		     setVisible(true);  
 			
-			
+		     try {
+	             File audioFile = new File("sukuna-screaming-for-help.wav");
+	             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+	             clip = AudioSystem.getClip();
+	             clip.open(audioStream);
+	             
+	             clip.stop(); // Stop the clip if it's already playing
+                 clip.setFramePosition(0); // Reset the clip to the beginning
+                 clip.start(); // Play the clip
+	             
+	         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+	             e.printStackTrace();
+	         }
 			
 		}
 
